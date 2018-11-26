2426e7a0c408868f48d4fab1614082c23cc3eeba
Cameron <cmori007@ucr.edu>
2018-11-26 12:48:23 -0800

    Testing login

--- client.py
+++ client.py
@@ -2,35 +2,49 @@ from getpass import getpass
 import socket
 import sys
 
-# try:
-#   s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+try:
+  s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# except socket.error:
-#   print 'Failed to create socket.'
-#   sys.exit()
+except socket.error:
+  print 'Failed to create socket.'
+  sys.exit()
 
-# host = 'localhost';
-# port = 8888
+host = 'localhost';
+port = 8888
 
-while(1) :
-  msg = raw_input('Enter a test message: ')
-  
+while (1):
   #Gather username and password
   username = raw_input('Please enter your username: ')
   password = getpass('Please enter your password: ')
+  try:
+    s.sendto((username, password), 1)
 
-  print password
-  break
+  except socket.error, msg:
+    print 'Error code: ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
 
-  # try:
-  #   s.sendto(msg, (host, port))
+  d = s.recvfrom(1024)
+  reply = d[0]
+  addr = d[1]
+
+  if reply == 'Valid Login':
+    break
+  else:
+    print 'Error: Invalid username or password.'
+
+
+# while(1) :
+#   msg = raw_input('Enter a test message: ')
+  
+#   try:
+#     s.sendto(msg, (host, port))
     
-  #   d = s.recvfrom(1024)
-  #   reply = d[0]
-  #   addr = d[1]
+#     d = s.recvfrom(1024)
+#     reply = d[0]
+#     addr = d[1]
 
-  #   print 'Server reply: ' + reply
+#     print 'Server reply: ' + reply
 
-  # except socket.error, msg:
-  #   print 'Error code: ' + str(msg[0]) + ' Message ' + msg[1]
-  #   sys.exit()
+#   except socket.error, msg:
+#     print 'Error code: ' + str(msg[0]) + ' Message ' + msg[1]
+#     sys.exit()
