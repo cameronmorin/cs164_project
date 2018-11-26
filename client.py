f646d3feb624f8d1b23869cdbe085389f1d002cd
Cameron <cmori007@ucr.edu>
2018-11-26 12:09:29 -0800

    Username and password input

--- client.py
+++ client.py
@@ -1,28 +1,36 @@
+from getpass import getpass
 import socket
 import sys
 
-try:
-  s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# try:
+#   s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-except socket.error:
-  print 'Failed to create socket.'
-  sys.exit()
+# except socket.error:
+#   print 'Failed to create socket.'
+#   sys.exit()
 
-host = 'localhost';
-port = 8888
+# host = 'localhost';
+# port = 8888
 
 while(1) :
   msg = raw_input('Enter a test message: ')
   
-  try:
-    s.sendto(msg, (host, port))
+  #Gather username and password
+  username = raw_input('Please enter your username: ')
+  password = getpass('Please enter your password: ')
+
+  print password
+  break
+
+  # try:
+  #   s.sendto(msg, (host, port))
     
-    d = s.recvfrom(1024)
-    reply = d[0]
-    addr = d[1]
+  #   d = s.recvfrom(1024)
+  #   reply = d[0]
+  #   addr = d[1]
 
-    print 'Server reply: ' + reply
+  #   print 'Server reply: ' + reply
 
-  except socket.error, msg:
-    print 'Error code: ' + str(msg[0]) + ' Message ' + msg[1]
-    sys.exit()
+  # except socket.error, msg:
+  #   print 'Error code: ' + str(msg[0]) + ' Message ' + msg[1]
+  #   sys.exit()
