4c980ba976e8bd22dbb56cd69002150bec697a5c
Cameron <cmori007@ucr.edu>
2018-11-27 11:19:16 -0800

    Test on lab machine

--- client.py
+++ client.py
@@ -9,30 +9,38 @@ except socket.error:
   print 'Failed to create socket.'
   sys.exit()
 
-host = 'localhost';
-port = 8888
+host = 'localhost'
+port = 6035
+
+try:
+  remote_ip = socket.gethostbyname(host)
+except socket.error, msg:
+  print 'Hostname could not be resolved. Exiting'
+  sys.exit()
+
+s.connect((host, port))
+print 'Socket connected to ' + host + ' on ip ' + remote_ip
 
 while (1):
   #Gather username and password
   username = raw_input('Please enter your username: ')
   password = getpass('Please enter your password: ')
   try:
-    s.sendto(((username, password), 1), (host, port))
+    s.sendall(username)
+    s.sendall(password)
 
   except socket.error, msg:
     print 'Error code: ' + str(msg[0]) + ' Message ' + msg[1]
     sys.exit()
 
-  d = s.recvfrom(1024)
-  reply = d[0]
-  addr = d[1]
+  reply = s.recv(4096)
 
   if reply == 'Valid Login':
     break
   else:
     print 'Error: Invalid username or password.'
 
-
+print 'Successful login'
 # while(1) :
 #   msg = raw_input('Enter a test message: ')
   
--- server.py
+++ server.py
@@ -1,50 +1,66 @@
-import socket
-import sys
+import socket, sys
 from thread import *
 
 HOST = ''
 PORT = 6035
 connections = []
-try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print 'Socket Created'
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind((HOST, PORT))
 
-except socket.error , msg:
-  print 'Bind failed. Error code : ' + str(msg[0]) + ' Message ' + msg[1]
-  sys.exit()
+credentials = [('cam', '1234'), ('harley', 'rock'), ('john', 'pass')]
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print 'Socket Created'
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+try:
+    s.bind((HOST, PORT))
+except socket.error, msg:
+    print 'Error code: ' + str(msg[0]) + ' Message ' + msg[1]
+    sys.exit()
 
 print 'Socket bind complete'
 
-s.listen(5)
+s.listen(10)
 print 'Socket now listening'
 
 def clientthread(conn):
-  conn.send('Welcome to the server. Type something and hit enter\n')
-
-  while True:
-    data = conn.recv(1024)
-    if (data[:2] == '!q'):
-      break
-    elif (data[:9] == '!sendall '):
-      reply = data[9:]
-      for item in connections:
-        item.sendall(reply)
-    else:
-      reply = 'OK...' + data
-      if not data:
-        break
-      conn.sendall(reply)
-
-
-  conn.close()
+    conn.send('Welcome to the server.')
+    
+    #User sign-on
+    while (1):
+        username = conn.recv(1024)
+        password = conn.recv(1024)
+        login = (username, password)
+        try:
+            cred = credentials.index(login)
+            reply = 'Valid Login'
+            conn.sendall(reply)
+            break
+        except ValueError:
+            invalid = 'Invalid credentials. Please try again'
+            conn.sendall(invalid)
+    
+    # #FIXME Print Menu
+    # while (1):
+    #     data = conn.recv(1024)
+    #     if (data[:2] == '!q'):
+    #         break
+    #     elif (data[:9] == '!sendall '):
+    #         reply = data[9:]
+    #         for client in connections:
+    #             client.sendall(reply)
+    #     else:
+    #         reply = 'OK...' + data
+    #         if not data:
+    #             break
+    #         conn.sendall(reply)
+    
+    conn.close()
 
 while (1):
-  conn, addr = s.accept()
-  connections.append(conn)
-  print 'Connected with ' + addr[0] + ':' + str(addr[1])
+    conn, addr = s.accept()
+    connections.append(conn)
+    print 'Connected with ' + addr[0] + ':' + str(addr[1])
 
-  start_new_thread(clientthread ,(conn,))
+    start_new_thread(clientthread ,(conn,))
 
 s.close()
