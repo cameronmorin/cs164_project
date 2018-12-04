48c3539c8ac5e21664dbde9875c55d5b5b4558f3
Cameron <cmori007@ucr.edu>
2018-12-04 14:33:18 -0800

    Implement User printFriends function

--- user.py
+++ user.py
@@ -6,10 +6,22 @@ class User:
         self.newMessages = []       #Used for real-time messages
         self.timeline = []
         self.friends = []
-        self.friendRequests = []
+        self.friendRequests = []    #[User object]
         self.onlineStatus = 0       #Default to offline
         self.connection = 0         #Defalut empty connection
 
     def changePassword(self, newPassword):
         self.password = newPassword
     
+    def printFriends(self):
+        num = 1
+        toSend = 'Your Friends:\n'
+        for friend in self.friends:
+            toSend += str(num) + ': ' + friend.username + '   Status: '
+            if friend.onlineStatus == 0:
+                #Offline
+                toSend += 'Offline\n'
+            else:
+                toSend += 'Online\n'
+        toSend += '-o'
+        return toSend
